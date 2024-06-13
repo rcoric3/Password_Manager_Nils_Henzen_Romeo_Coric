87e805511ef2ae4078015c7d93a218f9ef820a48
rcoric3 <92095914+rcoric3@users.noreply.github.com>
2024-06-13 14:42:29 +0200

    added reset button to login window

--- password_manager_frontend/src/pages/Login.js
+++ password_manager_frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useLocation } from "wouter";
+import ResetPasswordButton from "../components/PasswordResetButton";
 
 export default function Login() {
   const [username, setUsername] = useState("");
@@ -91,59 +92,62 @@ export default function Login() {
   };
 
   return (
-    <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-md">
-      <h2 className="text-center text-4xl">Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700 mb-1"
-          >
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div className="text-center">
-          <button
-            type="submit"
-            className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
-          >
-            Login
-          </button>
-          <button
-            type="button"
-            onClick={handleRegisterClick}
-            className="ml-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
-          >
-            Register
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <ResetPasswordButton />
+      <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-md">
+        <h2 className="text-center text-4xl">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username:
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              onClick={handleRegisterClick}
+              className="ml-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
